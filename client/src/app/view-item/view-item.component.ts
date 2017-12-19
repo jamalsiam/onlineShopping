9a1618ae278aa66e88610f9c8c23dc48d4ad2dd1
jamalsiam <jamalsiam1995@gmail.com>
2017-12-19 08:55:28 +0200

    view item UX/UI

---
+++ client/src/app/view-item/view-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-item',
+  templateUrl: './view-item.component.html',
+  styleUrls: ['./view-item.component.css']
+})
+export class ViewItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
