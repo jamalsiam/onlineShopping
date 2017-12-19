2607f9e35eade6bce7caebd03c675550bfc2f0e2
jamalsiam <jamalsiam1995@gmail.com>
2017-12-19 15:24:11 +0200

    connect to mongoose

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var session = require('express-session');
-var handlers = require('./handlers.js')
+
+const mongoose = require('mongoose');
+
+var handlers = require('./handlers.js');
 
 //middleware
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://jeme:123@ds115918.mlab.com:15918/markstore');
+var db = mongoose.connection;
 
 
 app.get('/api/getmethod',handlers.handelUser.getMethod);
