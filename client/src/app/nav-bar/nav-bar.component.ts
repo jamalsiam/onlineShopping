10228be0b21c12e561f4b55bffc6d78808f8e164
jamalsiam <jamalsiam1995@gmail.com>
2017-12-20 21:28:17 +0200

    nav-bar update /add username

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LocalStorageService} from 'angular-2-local-storage';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
-
+  username: string;
+  constructor(private storage: LocalStorageService , private service: DataService) {
+    this.service.getUserName({id: this.storage.get('onlineShopUserId')})
+      .subscribe(res => {
+        if (res.data === 'success') {
+          this.username = res.username;
+        } else {
+          this.storage.remove('onlineShopUserId');
+        }
+      });
+  }
   ngOnInit() {
   }
 
