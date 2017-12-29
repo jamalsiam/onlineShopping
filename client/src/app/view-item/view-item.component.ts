3ca1c22e8728c37e7784eb1575e5d7cb2706974d
jamalsiam <jamalsiam1995@gmail.com>
2017-12-30 01:04:32 +0200

    andary

--- client/src/app/view-item/view-item.component.ts
+++ client/src/app/view-item/view-item.component.ts
@@ -8,13 +8,21 @@ import {DataService} from '../data.service';
   styleUrls: ['./view-item.component.css']
 })
 export class ViewItemComponent implements OnInit {
-
+  item: any;
+  user: any;
   constructor(private route: ActivatedRoute , private service: DataService ) {
     this.route.params.subscribe(res => {
-      this.service.getItemInfo({id: res.id}).subscribe(res => { });
+      this.service.getItemInfo({id: res.id}).subscribe(res => {
+        this.item=res.info[0].item;
+        this.user=res.info[1].user;
+      });
     });
   }
 
+  
+  parseInt(num: string){
+    return parseInt( num );
+  }
   ngOnInit() {
   }
 
