1fa5e6586a5857a4ce12d27fd2479aa50933429b
jamalsiam <jamalsiam1995@gmail.com>
2017-12-30 02:17:56 +0200

    gallery

--- client/src/app/view-item/view-item.component.ts
+++ client/src/app/view-item/view-item.component.ts
@@ -10,16 +10,35 @@ import {DataService} from '../data.service';
 export class ViewItemComponent implements OnInit {
   item: any;
   user: any;
+  currentImage: string;
+  i: number=0;
+  images: any;
+  length:number;
   constructor(private route: ActivatedRoute , private service: DataService ) {
     this.route.params.subscribe(res => {
       this.service.getItemInfo({id: res.id}).subscribe(res => {
         this.item=res.info[0].item;
         this.user=res.info[1].user;
+        this.currentImage=res.info[0].item.images[this.i].image;
+        this.images=res.info[0].item.images;
       });
     });
   }
+  previousImage(){
+    this.i-=1;
+    if(this.i < 0)
+      this.i=this.item.images.length-1;
 
-  
+    this.currentImage=this.item.images[this.i].image
+  }
+  nextImage(){
+    this.i+=1;
+    if(this.i>this.item.images.length-1)
+      this.i=0;
+    console.log(this.item.images.length-1)
+    this.currentImage=this.item.images[this.i].image
+  }
+
   parseInt(num: string){
     return parseInt( num );
   }
