0de7d5c29986463a563c16490bf65bd29ebdd8d8
jamalsiam <jamalsiam1995@gmail.com>
2018-01-02 00:38:25 +0200

    scrollTop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+    this.jQuery();
+  }
+
+  jQuery(){
+    $(document).ready(function() {
+      var scroll=$('.scroll');
+      var heightScreen=$('.heightScreen').height()
+      scroll.hide();
+      $(window).scroll(function() {
+        if ($(document).scrollTop() >heightScreen) {
+          scroll.fadeIn('slow');
+        }
+        else {
+          scroll.fadeOut('slow');
+        }
+      });
+
+      scroll.click(function () {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+      })
+    });​
+  }
 
 }
