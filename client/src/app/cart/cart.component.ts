9bc30767050c95d8162e0311d0423ecd4a138636
jamalsiam <jamalsiam1995@gmail.com>
2018-01-31 16:33:28 +0200

    update cart and sale ux/ui

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -10,11 +10,14 @@ import {LocalStorageService} from "angular-2-local-storage";
 export class CartComponent implements OnInit {
   cart: any;
   display:boolean;
+  err:string;
   constructor(private storage: LocalStorageService, private service: DataService) {
-
+    this.err='';
     this.service.getCart({id: this.storage.get('onlineShopUserId')}).subscribe(res => {
       this.cart = res.cart;
       this.display=true;
+       if(res.cart.length==0)
+       this.err='You Don\'t Have Any Cart Item';
     });
   }
 
--- client/src/app/sale/sale.component.ts
+++ client/src/app/sale/sale.component.ts
@@ -10,11 +10,15 @@ import {DataService} from '../data.service';
 export class SaleComponent implements OnInit {
   sales: any;
   display:boolean;
+  err:string;
   constructor(private storage: LocalStorageService, private service: DataService) {
 
     this.service.getSale({id: this.storage.get('onlineShopUserId')}).subscribe(res => {
       this.sales = res.sales;
       this.display=true;
+      if(res.sales.length==0)
+      this.err='You Don\'t Have Any record at Publish Item';
+  
     });
   }
 
